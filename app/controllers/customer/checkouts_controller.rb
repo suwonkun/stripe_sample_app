8c6d91af1d222196e64492d3f939d489beda22a9
suwonkun <zhiyinzhongze@gmail.com>
2023-08-04 18:23:06 +0900

    手数料20%の設定・未キャプチャの決済・連結アカウントで決済

--- app/controllers/customer/checkouts_controller.rb
+++ app/controllers/customer/checkouts_controller.rb
@@ -11,12 +11,20 @@ class Customer::CheckoutsController < ApplicationController
   private
 
   def create_session(line_items)
-    Stripe::Checkout::Session.create(
+    # 売上の20%を手数料として設定
+    total_amount = line_items.sum do |item|
+      item[:quantity] * item[:price_data][:unit_amount]
+    end
+  
+    application_fee = (total_amount * 0.20).to_i
+
+  
+    Stripe::Checkout::Session.create({
       client_reference_id: current_customer.id,
       customer_email: current_customer.email,
       mode: 'payment',
       payment_method_types: ['card'],
-      line_items:,
+      line_items: line_items,
       shipping_address_collection: {
         allowed_countries: ['JP']
       },
@@ -32,8 +40,15 @@ class Customer::CheckoutsController < ApplicationController
           }
         }
       ],
+      payment_intent_data: {
+        capture_method: 'manual', #未キャプチャの支払いを作成
+        application_fee_amount: application_fee, # 手数料を設定
+      },
       success_url: "#{root_url}orders/success",
       cancel_url: "#{root_url}cart_items"
-    )
+    },
+    {
+      stripe_account: 'acct_1NaZA32fQhK2eMWA'  # Stripeの連結アカウントIDを指定します
+    })
   end
 end
